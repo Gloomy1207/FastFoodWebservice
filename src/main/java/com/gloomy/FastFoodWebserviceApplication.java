3e36848896313032be07fdc7ed0b39575864263f
Gloomy <glomknight@gmail.com>
2017-03-15 23:03:24 +0700

    Initialize commit

---
+++ src/main/java/com/gloomy/FastFoodWebserviceApplication.java
@@ -0,0 +1,12 @@
+package com.gloomy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFoodWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFoodWebserviceApplication.class, args);
+	}
+}
